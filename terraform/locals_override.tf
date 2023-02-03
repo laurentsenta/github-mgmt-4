45c978ea11b4cc024b65b7adfb1ba0ee75cfee84
Steve Loeppky <biglep@protocol.ai>
2023-02-02 19:07:40 -0800

    Manage more resource types except branch_protection

--- terraform/locals_override.tf
+++ terraform/locals_override.tf
@@ -1,6 +1,11 @@
 locals {
   resource_types = [
+    "github_membership",
+    "github_repository_collaborator",
     "github_repository",
-    "github_branch_protection"
+    "github_team_membership",
+    "github_team_repository",
+    "github_team",
+    "github_repository_file"
   ]
 }
