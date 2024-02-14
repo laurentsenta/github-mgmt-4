e20ca2a63b45f444bdfccf5d2c6c512349997e7a
Piotr Galar <piotr.galar@gmail.com>
2024-02-14 17:59:01 +0100

    Update terraform_override.tf

--- terraform/terraform_override.tf
+++ terraform/terraform_override.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     # account_id = "642361402189"
     region               = "us-east-1"
-    bucket               = "custom-github-runners-v0-filoz"
+    bucket               = "github-as-code-filoz"
     key                  = "terraform.tfstate"
     workspace_key_prefix = "org"
-    dynamodb_table       = "custom-github-runners-v0-filoz"
+    dynamodb_table       = "github-as-code-filoz"
   }
 }
