de80caabd45bd1bcfcb6263de71e1c395b9e04b5
Piotr Galar <piotr.galar@gmail.com>
2024-03-16 10:52:59 +0100

    Update resources.tf

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -211,7 +211,7 @@ resource "github_team_repository" "this" {
   ]
 
   repository = each.value.repository
-  team_id = contains(each.value, "team_id") ? each.value.team_id : github_team.this[lower(each.value.team)].id
+  team_id = contains(keys(each.value), "team_id") ? each.value.team_id : github_team.this[lower(each.value.team)].id
 
   permission = try(each.value.permission, null)
 
