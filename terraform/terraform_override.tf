29f65bac31ba76f1f3fb7a94b8009c95e14c8346
Piotr Galar <piotr.galar@gmail.com>
2024-09-11 12:52:36 +0200

    Update terraform_override.tf

--- terraform/terraform_override.tf
+++ terraform/terraform_override.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    # account_id = "642361402189"
     region               = "us-east-1"
     bucket               = "github-as-code-filoz"
     key                  = "terraform.tfstate"
